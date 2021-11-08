86018accaf0ea96a7d6669d8b4f423bbe868c8eb
Marc Blanco <marcblanco03@gmail.com>
2021-11-08 10:23:13 +0100

    Functions needed in appIbai and appNetflix

--- app/functions.py
+++ app/functions.py
@@ -13,12 +13,11 @@ import copy
 from st_aggrid import AgGrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 
-#allow_output_mutation=True para que la operación de cachear sea mucho más rápida
-#ya que no tiene que comprobar que el objeto cacheado es igual al calculado. Hay que ser más cuidadoso de no mutar el objeto.
+#allow_output_mutation=True for faster cacher. Make sure returned object is not modified
 @st.cache(allow_output_mutation=True) #static data
 def get_data(data_name,data_path):
 
-    #Directorios
+    #Directories
     preprocessed_data_path='{}/preprocessed_data/{}_preprocessed.pq'.format(data_path,data_name)
     sentiment_data_path='{}/sentiment_data/{}_sentiment.npy'.format(data_path,data_name)
     topic_data_path='{}/topic_data/{}_topics.json'.format(data_path,data_name)
@@ -26,34 +25,31 @@ def get_data(data_name,data_path):
     df=pd.read_parquet(preprocessed_data_path)
     df.drop(['inReplyToTweetId','inReplyToUser','quotedTweet'],axis=1,inplace=True)
 
-    #Cargo la predicción de cada fila, cambio el array numérico a palabras y añado al dataframe
+    #Load sentiment of tweets
     with open(sentiment_data_path,'rb') as f:
         sentiments=np.load(f)
     num_to_sentiment={-1:'Negativa',0:'Neutral',1:'Positiva'}
     sentiments=[num_to_sentiment[sentiment] for sentiment in sentiments]
     df['Opinión']=sentiments 
 
-    #Cargo el json de topics
+    #Load topics
     with open(topic_data_path,'rb') as f:
         topics=json.load(f) 
-    #Las palabras que pueden definir a los topics
+    #Terms for topics
     vocab=topics['vocab'] 
-    #Se asigna el cluster que tenga mayor probabilidad   
+    #Choose cluster/topic with greatest probability and its probability
     df['Cluster']=np.argmax(topics['doc_topic_dists'],axis=1)
-    #Se guarda también esa probabilidad
     df['Cluster_prob']=np.max(topics['doc_topic_dists'],axis=1)
-    N=30 #número de palabras representando a cada topic.
-    #topics['topic_term_dists'] es array de tamaño numero_topics,len(vocab) y lo que hago es asignar a cada cluster
-    #las palabras con mayor probabilidad
+    N=30 #Number of words representing each cluster
+    #Choose the closest N words for each cluster and save them in topic_terms, index n corresponds to cluster n
     indices_words=np.flip(np.argsort(topics['topic_term_dists'], axis=1),axis=1)[:,:N]
-    #Hago lista de listas de palabras. La primera lista corresponde a las palabras del cluster 0
     topic_terms=[]
     for i in indices_words:
         tmp=[]
         for j in i:
             tmp.append(vocab[j])
         topic_terms.append(tmp)
-    #Frecuencia de los términos
+    
     term_frequency=topics['term_frequency']  
 
     return df,topic_terms,vocab,term_frequency
@@ -68,14 +64,14 @@ def df_filtered(df,start_date,end_date,text_in_tweet,clusters,langs,outlinks,sou
     -rest of parameters are lists in order to filter
     Returns: pandas dataframe
     """
-    #Filtro de fecha
+    #Date filter
     df_filtrado=df[(df['date']> "{}".format(start_date)) & 
             (df['date']< "{}".format(end_date))]
-    #Filtro de texto
+    #text filter
     if text_in_tweet.strip()!='':
         df_filtrado=df_filtrado[(df_filtrado['content_embeddings'].apply(lambda x: text_in_tweet.lower().strip() in str(x).lower()))
                                 |(df_filtrado['content_tokens'].apply(lambda x: text_in_tweet.lower().strip() in str(x).lower()))]
-    #Filtros para las columnas correspondientes. Si están vacias, no lo compruebo.
+    #Rest of filters
     if len(clusters)!=0:                            
         df_filtrado=df_filtrado[df_filtrado['Cluster'].isin(clusters)] 
     if len(langs)!=0:
@@ -99,34 +95,33 @@ def cluster_figure(df_orig,df,topic_terms,colores,pos_to_cluster):
     -pos_to_cluster: dict mapping cluster number to an index in colores
     Returns: plotly figure
     """  
-    #Value counts de df original
+    #Value counts of original df 
     cluster_counts_orig=df_orig['Cluster'].value_counts(ascending=True)
-    #Value counts de df, se pasa a dict y se añaden los clusters que no estén con el valor 0
+    #Value counts of current df. Add missing clusters with value 0
     cluster_counts_tmp=df['Cluster'].value_counts()
     cluster_counts_tmp=dict(zip(cluster_counts_tmp.keys().tolist(),cluster_counts_tmp.tolist()))
     for i in list(pos_to_cluster.keys()):
         if i not in cluster_counts_tmp.keys():
             cluster_counts_tmp[i]=0
-    #Se reordenan los valores de los clusters según las posiciones de pos_to_cluster que deben ser 
-    #las mismas que en cluster_counts_orig
+    #Order clusters according to pos_to_cluster q
     cluster_counts_nuevos=[]
     for i in range(len(pos_to_cluster)):
         cluster_counts_nuevos.append(cluster_counts_tmp[pos_to_cluster[i]])
 
-    #Añado los value_counts del df original con una opacidad reducida
+    #Reduced opacity for original df
     trace0=go.Bar(y=[str(i) for i in pos_to_cluster.values()],
             x=list(cluster_counts_orig),
             orientation='h',
             marker_color=colores,name='original').update(opacity=0.25)
 
-    #Añado los value_counts del df 
+    #Value counts of current df
     trace1=go.Bar(y=[str(i) for i in pos_to_cluster.values()],
                 x=list(cluster_counts_nuevos),
                 orientation='h',
                 marker_color=colores,name='filtrado')
     data=[trace0,trace1]
 
-    #Añado las palabras de cada cluster
+    #Add list of words representing each cluster
     annotations = [dict(
                 x=0.1,
                 y=y,
@@ -136,7 +131,7 @@ def cluster_figure(df_orig,df,topic_terms,colores,pos_to_cluster):
                 showarrow=False,
                 font=dict(color="#ffffff")
             ) for y,cluster in pos_to_cluster.items()]
-    #Arreglo layout para margenes, título,colores...
+    #Layout...
     layout=go.Layout(title='Clusters detectados',title_x=0.5,title_y=0.93,xaxis_title="Tweets",
                         yaxis_title="Clusters",barmode='overlay',
                         legend=dict(itemclick="toggleothers",itemdoubleclick="toggle",xanchor='auto',yanchor='auto',x=1,y=.05),#Adjust click behavior
@@ -161,30 +156,31 @@ def hourday_figure(df_orig,df):
     -df_orig: dataframe to compare with df
     Returns: plotly figure
     """
-    #Para df_orig
-    hourday_counts_orig=df_orig.groupby(df_orig['date'].dt.hour).size() #Distribucion de las filas a lo largo de las horas del dia
-    hourday_counts_orig_dict=dict(zip(hourday_counts_orig.index.astype('int').tolist(),hourday_counts_orig.values.tolist()))#Lo paso a diccionario
-    #Para df
-    hourday_counts=df.groupby(df['date'].dt.hour).size() #Hora del día
-    hourday_counts_dict=dict(zip(hourday_counts.index.astype('int').tolist(),hourday_counts.values.tolist()))#Lo paso a diccionario
-    #Relleno con 0's las horas que no estén
+    #distribution of original df
+    hourday_counts_orig=df_orig.groupby(df_orig['date'].dt.hour).size() 
+    #Convert to dict
+    hourday_counts_orig_dict=dict(zip(hourday_counts_orig.index.astype('int').tolist(),hourday_counts_orig.values.tolist()))
+    #distribution of current df
+    hourday_counts=df.groupby(df['date'].dt.hour).size() 
+    hourday_counts_dict=dict(zip(hourday_counts.index.astype('int').tolist(),hourday_counts.values.tolist()))
+    #Fill missing hours with 0
     for i in range(24):
         if i not in hourday_counts_orig.keys():
             hourday_counts_orig_dict[i]=0
         if i not in hourday_counts.keys():
             hourday_counts_dict[i]=0
-    #Figura
-    #Añado el original
+    #Figure
+    #Original df
     trace0=go.Bar(y=list(hourday_counts_orig_dict.values()),
             x=list(hourday_counts_orig_dict.keys()),
             marker_color='#1d9bf0',name='original'
             ).update(opacity=0.25)
-    #Añado el del df
+    #current df
     trace1=go.Bar(y=list(hourday_counts_dict.values()),
                 x=list(hourday_counts_dict.keys()),
                 marker_color='#1d9bf0',name='filtrado'
             )
-    #Cambio layout
+    #layout...
     layout=go.Layout(title='Tweets a lo largo del día',title_x=0.5,barmode='overlay',margin=go.layout.Margin(l=0,r=0, b=0,t=50),
                      xaxis_title="h",height=275,legend=dict(itemclick="toggleothers",itemdoubleclick="toggle",xanchor='auto',yanchor='middle',bgcolor='rgba(0,0,0,0)'),
            )
@@ -200,31 +196,31 @@ def dayofweek_figure(df_orig,df):
     -df_orig: dataframe to compare with df
     Returns: plotly figure
     """
-    #dict para cambiar número devueltos por pandas por las letras de los días
+    
     num_to_day={0:'L',1:'M',2:'X',3:'J',4:'V',5:'S',6:'D'}
-    #Invierto orden para la figura
-    dayofweek_counts_orig=df_orig.groupby(df_orig['date'].dt.dayofweek).size()[::-1]#Distribucion
-    #Lo paso a diccionario
+    #Distribution over the days and reverse order
+    dayofweek_counts_orig=df_orig.groupby(df_orig['date'].dt.dayofweek).size()[::-1]
+    #to dict
     dayofweek_counts_orig_dict=dict(zip(dayofweek_counts_orig.index.tolist(),dayofweek_counts_orig.values.tolist()))
-    
+    #Current df
     dayofweek_counts=df.groupby(df['date'].dt.dayofweek).size()[::-1] 
-    dayofweek_counts_dict=dict(zip(dayofweek_counts.index.tolist(),dayofweek_counts.values.tolist()))#Lo paso a diccionario
+    dayofweek_counts_dict=dict(zip(dayofweek_counts.index.tolist(),dayofweek_counts.values.tolist()))
     
-    #Relleno con 0's las días que no estén
+    #Fill missing days with 0
     for i in range(7):
         if i not in dayofweek_counts_orig.keys():
             dayofweek_counts_orig_dict[i]=0
         if i not in dayofweek_counts.keys():
             dayofweek_counts_dict[i]=0
 
-    #Figuras
-    y_orig=[num_to_day[i] for i in list(dayofweek_counts_orig_dict.keys())] #Cambio de números a letras
+    #Figures
+    y_orig=[num_to_day[i] for i in list(dayofweek_counts_orig_dict.keys())] #Numbers to letters
     trace0=go.Bar(y=y_orig,
             x=list(dayofweek_counts_orig_dict.values()),
             marker_color='#1d9bf0',name='original',orientation='h'
             ).update(opacity=0.25)
 
-    y=[num_to_day[i] for i in list(dayofweek_counts_dict.keys())] #Cambio de números a letras
+    y=[num_to_day[i] for i in list(dayofweek_counts_dict.keys())] 
     trace1=go.Bar(y=y,
                 x=list(dayofweek_counts_dict.values()),
                 marker_color='#1d9bf0',name='filtrado',orientation='h'
@@ -241,7 +237,7 @@ def dayofweek_figure(df_orig,df):
 
     return dayofweek_figure
 
-#Función necesaria para expandir las de columnas con listas y hacer hacer value counts
+#Function needed to expand columns with lists
 def to_1D(series):
     return pd.Series([x for _list in series for x in _list])
 
@@ -252,7 +248,7 @@ def value_counts_figure(df,columna,top,title):
     -top: number of categorias to show in histogram
     Returns: plotly figure
     """
-    #Si la columna es una de estas, es una columna de listas y hay que expandirla
+    #This columns have to be expanded
     if columna in ['hashtags','mentionedUsers','cashtags','outlinks']:
         tmp=to_1D(df[columna])
     else:
@@ -265,7 +261,7 @@ def value_counts_figure(df,columna,top,title):
                     paper_bgcolor='#15202b',
                     font = dict(color = '#ffffff'), 
                     #width=500,
-                    height=len(column_counts)*12+104 #Un mínimo para cuando hay una sola barra que no se corte la figura
+                    height=len(column_counts)*12+104
                     )
     column_counts_fig= go.Figure(go.Bar(y=column_counts.index,x=list(column_counts),
                                         orientation='h',marker_color='#1d9bf0'
@@ -283,27 +279,27 @@ def temporal_fig(df_filtered,cluster_to_pos,clusters_filtro,colores):
     -cluster_to_pos: dict with cluster number to index in colores list
     Returns: plotly figure
     """
-    #Añado el cluster -1 para repsentar a todo el dataframe si la lista está vacía
+    #Cluster -1 represents the entire df if list is empty
     NumClusters=copy.deepcopy(clusters_filtro)
     if len(clusters_filtro)==0:
         NumClusters.append(-1)  
 
     df=df_filtered.copy(deep=True)
     df=df.set_index('date')
-    #Columnnas para enseñar
+    #Columnns to show
     cols=['replyCount','retweetCount','likeCount','quoteCount','Photo','Video','Cluster']
     df=df[cols]
     df.columns=['Respuestas','Retweets','Likes','Citas','Fotos','Videos','Cluster']
-    #Dependiendo los limites temporales del df, agrupo por años o días. Por semanas y por meses siempre lo hago
+    #Group by days or years depending of maximum time difference
     if (df.index.max()-df.index.min())>datetime.timedelta(days=365):
-        escalas=['Y','M','W']
+        escalas=['Y','M','W'] #Year,month, week
     else:
-        escalas=['M','W','D']
+        escalas=['M','W','D'] #Month,week,day
 
     fig=go.Figure()
     dfs=[]
-    #Bucle para cada cluster y escala de tiempo. Tengo que dibujar todas. Habrá un butón para ocultar las escalas temporales
-    #que no queramos. Habrá otro botón para elegir la columna y cambiar las curvas que hay.
+    
+    #Loop for drawing every cluster and time scale. It will be a button to hide unwanted time scales
     for cluster in NumClusters:
         if cluster==-1:
             color='#1d9bf0' 
@@ -311,8 +307,8 @@ def temporal_fig(df_filtered,cluster_to_pos,clusters_filtro,colores):
             color=colores[cluster_to_pos[cluster]]
         for escala in escalas:
             if cluster==-1:
-                df_tmp=df.resample(escala).mean()#Agrupo por la escala temporal y hago la media de cada filas
-                df_tmp.insert(0,'Tweets',df.resample(escala).size()) #Añado el número de filas para cada escala
+                df_tmp=df.resample(escala).mean()#Group by time scale and compute mean of every column
+                df_tmp.insert(0,'Tweets',df.resample(escala).size()) #Number of rows/tweets
             else:
                 df_tmp=df[df.Cluster==cluster].resample(escala).mean()
                 df_tmp.insert(0,'Tweets',df[df.Cluster==cluster].resample(escala).size())
@@ -337,7 +333,7 @@ def temporal_fig(df_filtered,cluster_to_pos,clusters_filtro,colores):
                         hoverinfo=hoverinfo
                         )
             )
-    #Botón para elegir que columna mostrar. Hay que añadir diccionarios por cada columna a la lista
+    #Button to select which column you want to see
     buttonsCol=[]
     col_to_label=dict(Tweets='Número de tweets',Respuestas='Media de Respuestas',Retweets='Media de Retweets',
                     Likes='Media de Likes',Citas='Media de Citas',Fotos='Tweets con Fotos (%)',Videos='Tweets con Videos (%)'
@@ -355,10 +351,9 @@ def temporal_fig(df_filtered,cluster_to_pos,clusters_filtro,colores):
                                   list(range(len(dfs)))]
                             )
                       )
-    #Botón para invisibilizar las escalas de tiempo que no queremos 
-    #Las trazas se ordenan de tal forma que si tenemos 2 clusters y 3 escalas temporales, la lista es
-    #[1Y,1M,1D,2Y,2M,2D] con los números representando el cluster y las letras las escalas. Si queremos mostrar los meses,
-    #la lista de visibility será [False,True,False,False,True,False]
+    #Button to hide time scales
+    #Traces are ordered in a way that if we have 2 clusters and 3 time scales, the order is [1Y,1M,1D,2Y,2M,2D]. 
+    #If we want to show months, visibility list is as follows [False,True,False,False,True,False]
     buttonsVisible = []
     escala_to_label=dict(Y='Año',M='Mes',W='Semana',D='Dia')
     for i,escala in enumerate(escalas):
@@ -367,7 +362,7 @@ def temporal_fig(df_filtered,cluster_to_pos,clusters_filtro,colores):
         visibility=visibility*len(NumClusters)
         buttonsVisible.append(dict(label=escala_to_label[escala],method = 'restyle',args = ['visible',visibility]))
 
-    #Añado los botones al updatemenu con diccionarios para especificar varios parámetros
+    #Add buttons to updatemenu
     updatemenu = []
 
     menuCol = dict(buttons=buttonsCol,direction='down',showactive=True,x=1,yanchor='bottom',xanchor='right',
@@ -397,14 +392,14 @@ def violin_distribution(df_filtered,column,clusters_filtro,cluster_to_pos,colore
     """
     df=df_filtered.copy(deep=True)
     violin_fig=go.Figure()
-    #Si no hay varios clusters a considerar, dibujo la distribución para todo el df
+    #If there are no clusters, draw distributions for the entire df
     if len(clusters_filtro)==0:
         color='#1d9bf0'
         violin_fig.add_trace(go.Violin(y=df[column]
                             ,name='Total',line_color=color,hoverinfo='x+y'
                             )
         )
-    #Si no, para cada cluster
+    #Draw distributions for each cluster
     else:
         for cluster in clusters_filtro:
             color=colores[cluster_to_pos[cluster]]
@@ -414,7 +409,7 @@ def violin_distribution(df_filtered,column,clusters_filtro,cluster_to_pos,colore
             )
 
     violin_fig.update_traces(orientation='v',side='both', width=1, points=False)
-    #Cambio rango de 'y' para mejorar la figura con distribuciones con grandes outliers como pueden ser los retweets 
+    #Limit y range because of outlier
     max_y=df[column].quantile(0.995)
     min_y=df[column].min()-10
 
@@ -434,16 +429,16 @@ def representative_tweets(df_tmp):
     Returns: plotly figure
     """
     df=df_tmp.copy(deep=True)
-    #Las tres columnas a considerar
+    #We only need these 3 columns
     df=df[['renderedContent','Cluster','Cluster_prob']]
     df.sort_values(by=['Cluster_prob'],ascending=False,inplace=True)
-    #Limito el número de filas a mostrar  
+    #Maximum rows to show 
     longitud=df.Cluster.unique().size*20
     if longitud>50:
         longitud=50
 
     df=df.round({'Cluster_prob':2}).head(longitud)
-    #Figura de la tabla
+    #Table
     fig = go.Figure(data=[go.Table(
     columnwidth = [0.76,0.1,0.14],
     header=dict(values=['Tweets','Cluster','Confianza'],
@@ -463,11 +458,11 @@ def relevant_terms_table(df):
     Value counts of words for a column of texts    
     Returns: plotly figure
     """
-    #Limito el número de filas
+    #Maximum number of rows
     longitud=df.Cluster.unique().size*15
     if longitud>50:
         longitud=50
-    #Uno la columna, separo por espacios y hago value_counts
+    #Merge rows of the column, split by spaces and value_counts so we have term frequencies.
     df_fig=pd.Series(' '.join(df.content_tokens).split()).value_counts().head(longitud)
     tokens_fuera=[x for x in ['fotos_y_videos'] if x in df_fig.index]
     df_fig.drop(tokens_fuera,axis=0,inplace=True)
@@ -494,13 +489,13 @@ def df_tabla(df_tmp):
     """
     df=df_tmp.copy(deep=True)
     df=df.round({'Cluster_prob':2})
-    #Filtro columnas y cambio nombres
+    #Selected columns
     df=df[['url','renderedContent','date','retweetCount','replyCount','quoteCount','Cluster','Cluster_prob','Opinión']] 
     df.columns=['Url','Tweet','Fecha','Retweets','Respuestas','Citas','Cluster','Confianza','Opinión']
     gb = GridOptionsBuilder.from_dataframe(df)
     gb.configure_pagination()
-    #Parametros que me gustaria personalizar y no funcionan
-    gb.configure_default_column(copyHeadersToClipboard=True,filterable=False,editable=False,resizable=False, groupable=False,)
+    #Parameters I would like to use but it doesnt work
+    #gb.configure_default_column(copyHeadersToClipboard=True,filterable=False,editable=False,resizable=False, groupable=False,)
 
     gridOptions = gb.build()
     AgGrid(df,gridOptions=gridOptions,enable_enterprise_modules=True)
